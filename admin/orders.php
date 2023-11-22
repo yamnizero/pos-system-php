af976f1ccb0228e36ef32346a1c6e03c1a1ba5ea
mohammed jamal <yamni.zero@gmail.com>
2023-11-22 21:23:38 +0200

    view order

--- admin/orders.php
+++ admin/orders.php
@@ -35,7 +35,7 @@
                                     <td ><?= $orderItem['order_status'] ?></td>
                                     <td ><?= $orderItem['payment_mode'] ?></td>
                                     <td>
-                                        <a href="" class="btn btn-info mb-0 px-2 btn-sm">View</a>
+                                        <a href="orders-view.php?track=<?= $orderItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm">View</a>
                                         <a href="" class="btn btn-primary mb-0 px-2 btn-sm">Print</a>
                                     </td>
                                 </tr>
