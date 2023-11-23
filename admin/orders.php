e0b33de01056a9a5c942c4a9ebdf30a91af7460e
mohammed jamal <yamni.zero@gmail.com>
2023-11-23 11:33:29 +0200

    print page

--- admin/orders.php
+++ admin/orders.php
@@ -36,7 +36,7 @@
                                     <td ><?= $orderItem['payment_mode'] ?></td>
                                     <td>
                                         <a href="orders-view.php?track=<?= $orderItem['tracking_no']; ?>" class="btn btn-info mb-0 px-2 btn-sm">View</a>
-                                        <a href="" class="btn btn-primary mb-0 px-2 btn-sm">Print</a>
+                                        <a href="orders-view-print.php?track=<?= $orderItem['tracking_no']; ?>" class="btn btn-primary mb-0 px-2 btn-sm">Print</a>
                                     </td>
                                 </tr>
                             <?php endforeach; ?>
