4909a3c2db202839441b58f2137ebd9d2989bc30
mohammed jamal <yamni.zero@gmail.com>
2023-11-23 11:35:54 +0200

    mybillingArea

--- admin/orders-view-print.php
+++ admin/orders-view-print.php
@@ -9,44 +9,45 @@
             </h4>
         </div>
         <div class="card-body">
-            <?php
-            if (isset($_GET['track'])) {
-                $trackingNo = validate($_GET['track']);
-                if ($trackingNo == '') {
-                     ?>
-                            <div class="text-center py-5">
-                                 <h5>Please provide Tarcking Number</h5>
-                                <div>
-                                 <a href="orders.php" class="btn btn-primary mt-4 w-25">Go back to Orders</a>
-                                 </div>
+            <div id="mybillingArea">
+                <?php
+                if (isset($_GET['track'])) {
+                    $trackingNo = validate($_GET['track']);
+                    if ($trackingNo == '') {
+                ?>
+                        <div class="text-center py-5">
+                            <h5>Please provide Tarcking Number</h5>
+                            <div>
+                                <a href="orders.php" class="btn btn-primary mt-4 w-25">Go back to Orders</a>
                             </div>
-                           
+                        </div>
+
                     <?php
-                }
+                    }
 
-                $orderQuery = "SELECT o.*,c.* FROM orders o, customers c WHERE c.id = o.customer_id AND tracking_no='$trackingNo' LIMIT 1";
-                $orderQueryRes = mysqli_query($conn,$orderQuery);
-                if (!$orderQueryRes) {
-                   echo "<h5>Something Went Wrong</h5>";
-                   return false;
-                }
+                    $orderQuery = "SELECT o.*,c.* FROM orders o, customers c WHERE c.id = o.customer_id AND tracking_no='$trackingNo' LIMIT 1";
+                    $orderQueryRes = mysqli_query($conn, $orderQuery);
+                    if (!$orderQueryRes) {
+                        echo "<h5>Something Went Wrong</h5>";
+                        return false;
+                    }
 
-                if (mysqli_num_rows($orderQueryRes) > 0) {
-                    $orderDataRow = mysqli_fetch_assoc($orderQueryRes);
-                    // print_r($orderDataRow);
-                    ?> 
+                    if (mysqli_num_rows($orderQueryRes) > 0) {
+                        $orderDataRow = mysqli_fetch_assoc($orderQueryRes);
+                        // print_r($orderDataRow);
+                    ?>
                         <table style="width:100%; margin-bottom:20px;">
                             <tbody>
                                 <tr>
                                     <td style="text-align:center;" colspan="2">
                                         <h4 style="font-size: 23px; line-height: 30px;margin: 2px;padding: 0;">Company Yamni</h4>
                                         <p style="font-size: 16px; line-height: 24px;margin: 2px;padding: 0;">#555, 1st, aldis, yemen</p>
-                                         <p style="font-size: 16px; line-height: 24px;margin: 2px;padding: 0;">Company Yamni pvt lyd.</p>
+                                        <p style="font-size: 16px; line-height: 24px;margin: 2px;padding: 0;">Company Yamni pvt lyd.</p>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td>
-                                       <h5 style="font-size: 20px; line-height: 30px; margin: 0px;padding: 0;">Customer Details</h5>
+                                        <h5 style="font-size: 20px; line-height: 30px; margin: 0px;padding: 0;">Customer Details</h5>
                                         <p style="font-size: 14px; line-height: 20px;margin: 0px;padding: 0;">Customer Name: <?= $orderDataRow['name'] ?></p>
                                         <p style="font-size: 14px; line-height: 20px;margin: 0px;padding: 0;">Customer Phone No: <?= $orderDataRow['phone'] ?></p>
                                         <p style="font-size: 14px; line-height: 20px;margin: 0px;padding: 0;">Customer Email Id: <?= $orderDataRow['email'] ?></p>
@@ -61,23 +62,23 @@
                             </tbody>
 
                         </table>
-                    <?php
-                } else {
-                    echo "<h5>No Date Found </h5>";
-                    return false;
-                }
+                        <?php
+                    } else {
+                        echo "<h5>No Date Found </h5>";
+                        return false;
+                    }
 
-                $orderItemQuery = "SELECT oi.quantity as orderItemQuantity, oi.price as orderItemPrice, o.*,oi.*,p.* FROM orders o, order_items oi, products p WHERE oi.order_id =o.id AND p.id=oi.product_id AND o.tracking_no='$trackingNo'";
-                $orderItemQueryRes = mysqli_query($conn,$orderItemQuery);
-                if ($orderItemQueryRes) {
-                    if (mysqli_num_rows($orderItemQueryRes) > 0) {
+                    $orderItemQuery = "SELECT oi.quantity as orderItemQuantity, oi.price as orderItemPrice, o.*,oi.*,p.* FROM orders o, order_items oi, products p WHERE oi.order_id =o.id AND p.id=oi.product_id AND o.tracking_no='$trackingNo'";
+                    $orderItemQueryRes = mysqli_query($conn, $orderItemQuery);
+                    if ($orderItemQueryRes) {
+                        if (mysqli_num_rows($orderItemQueryRes) > 0) {
                         ?>
                             <div class="table-responsive mb-3">
                                 <table style="width:100%;" cellpadding="5">
                                     <thead>
                                         <tr>
                                             <th align="start" style="border-bottom:1px solid #ccc;" width="5%">ID</th>
-                                            <th align="start" style="border-bottom:1px solid #ccc;" >Product Name</th>
+                                            <th align="start" style="border-bottom:1px solid #ccc;">Product Name</th>
                                             <th align="start" style="border-bottom:1px solid #ccc;" width="10%">Price</th>
                                             <th align="start" style="border-bottom:1px solid #ccc;" width="10%">Quantity</th>
                                             <th align="start" style="border-bottom:1px solid #ccc;" width="15%">Total price</th>
@@ -85,57 +86,54 @@
                                     </thead>
                                     <tbody>
                                         <?php
-                                            $i =1;
-                                            
-                                            foreach($orderItemQueryRes as $key =>$row) :
-                                            
+                                        $i = 1;
+
+                                        foreach ($orderItemQueryRes as $key => $row) :
+
                                         ?>
-                                        <tr>
-                                            <td style="border-bottom:1px solid #ccc;"><?= $i++;?></td>
-                                            <td style="border-bottom:1px solid #ccc;"><?=$row['name'];?></td>
-                                            <td style="border-bottom:1px solid #ccc;"><?= number_format($row['orderItemPrice'],0)?></td>
-                                            <td style="border-bottom:1px solid #ccc;"><?=$row['orderItemQuantity']?></td>
-                                            <td style="border-bottom:1px solid #ccc;" class="fw-bold">
-                                            <?= number_format($row['orderItemPrice'] * $row['orderItemQuantity'],0)?>
-                                        </td>
-                                        </tr>
+                                            <tr>
+                                                <td style="border-bottom:1px solid #ccc;"><?= $i++; ?></td>
+                                                <td style="border-bottom:1px solid #ccc;"><?= $row['name']; ?></td>
+                                                <td style="border-bottom:1px solid #ccc;"><?= number_format($row['orderItemPrice'], 0) ?></td>
+                                                <td style="border-bottom:1px solid #ccc;"><?= $row['orderItemQuantity'] ?></td>
+                                                <td style="border-bottom:1px solid #ccc;" class="fw-bold">
+                                                    <?= number_format($row['orderItemPrice'] * $row['orderItemQuantity'], 0) ?>
+                                                </td>
+                                            </tr>
                                         <?php endforeach; ?>
                                         <tr>
                                             <td colspan="4" align="end" style="font-weight:bold;">Grand Total: </td>
-                                            <td colspan="1"  style="font-weight:bold;"><?= number_format($row['total_amount'],0)?></td>    
+                                            <td colspan="1" style="font-weight:bold;"><?= number_format($row['total_amount'], 0) ?></td>
                                         </tr>
                                         <tr>
-                                            <td colspan="5">Payment Mode: <?=$row['payment_mode'];?> </td>
-                                            
+                                            <td colspan="5">Payment Mode: <?= $row['payment_mode']; ?> </td>
+
                                         </tr>
                                     </tbody>
 
                                 </table>
                             </div>
-                        <?php
+                    <?php
+                        } else {
+                            echo "<h5>No Data Found</h5>";
+                            return false;
+                        }
                     } else {
-                        echo "<h5>No Data Found</h5>";
+                        echo "<h5>Something Went Wrong</h5>";
                         return false;
                     }
-                    
                 } else {
-                    echo "<h5>Something Went Wrong</h5>";
-                    return false;
-                }
-                
-
-            } else {
-                ?>
-                <div class="text-center py-5">
-                    <h5>No Tarcking Number Parameter Found</h5>
-                    <div>
-                        <a href="orders.php" class="btn btn-primary mt-4 w-25">Go back to Orders</a>
+                    ?>
+                    <div class="text-center py-5">
+                        <h5>No Tarcking Number Parameter Found</h5>
+                        <div>
+                            <a href="orders.php" class="btn btn-primary mt-4 w-25">Go back to Orders</a>
+                        </div>
                     </div>
-                </div>
-            <?php
-            }
-            ?>
-
+                <?php
+                }
+                ?>
+            </div>
 
         </div>
 
